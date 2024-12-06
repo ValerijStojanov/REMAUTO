36703ccdde14fe7a5acc11d1e709ca8db0fb296f
Val_St <stojanov.valerij@seznam.cz>
2024-12-06 18:48:24 +0100

    add first view

--- remauto/settings.py
+++ remauto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*g8=^midz(#0rng_v0x@p@(c^v^u6g6ns-&9jz_vuc!gtk@_8q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['AlexRem.pythonanywhere.com']
+ALLOWED_HOSTS = ['AlexRem.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'remauto_crm_system',
 ]
 
 MIDDLEWARE = [
---
+++ remauto_crm_system/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RemautoCrmSystemConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'remauto_crm_system'
